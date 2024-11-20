a7a17a1795db8b96518ae99ab0eab0329436a614
denis springinsfeld <denis.springinsfeld@unilim.fr>
2024-11-20 17:51:03 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind()],
+  site: "https://denis-springinsfeld.github.io",
+  base: "astroblog",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
